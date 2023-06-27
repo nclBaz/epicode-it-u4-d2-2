30ae9d2adf79cb54dc5df95b1a18feefd12bc2b9
Riccardo Gulin <riccardo.gulin@gmail.com>
2023-06-27 12:38:43 +0200

    Adds equals & toString examples

--- src/u4d2/App.java
+++ src/u4d2/App.java
@@ -2,22 +2,65 @@ package u4d2;
 
 public class App {
 	public static void main(String[] args) {
+//		Student aldo = new Student("Aldo", "Baglio");
+//		aldo.sayHello();
+//
+//		Student giovanni = new Student("Giovanni", "Storti");
+//		giovanni.sayHello();
+//
+//		Student giacomo = aldo;
+//		giacomo.surname = "Poretti";
+//		giacomo.sayHello();
+//
+//		aldo.sayHello();
+//
+//		Math.sqrt(4);
+//		System.out.println(Math.PI);
+//
+//		Student.staticMethod();
+//		System.out.println(Student.school);
+
+		// ----------- COMPARAZIONE TRA NUMERI ---------------
+
+//		byte x = 2;
+//		int y = 2;
+//		double z = 2.0;
+//
+//		if (x == z) {
+//			System.out.println("I due numeri sono uguali");
+//		}
+
+		// ----------- COMPARAZIONE TRA STRINGHE ---------------
+//		String s1 = "ciao";
+//		String s2 = "ciao";
+//		s2 = "Arrivederci";
+//		String s3 = new String("ciao");
+//
+//		if (s1.equals(s3)) {
+//			System.out.println("Le Stringhe sono uguali");
+//		} else {
+//			System.out.println("Le Stringhe sono diverse");
+//		}
+
+		// ----------- COMPARAZIONE TRA OGGETTI ---------------
+
+//		Student aldo = new Student("Aldo", "Baglio");
+//		Student aldo2 = new Student("Aldo", "Baglio");
+//		Student aldo3 = aldo;
+//
+//		if (aldo.equals(aldo2)) {
+//			System.out.println("Gli studenti sono uguali");
+//		} else {
+//			System.out.println("Gli studenti sono diversi");
+//		}
+
+		// -------------------- TO STRING ---------------------
 		Student aldo = new Student("Aldo", "Baglio");
-		aldo.sayHello();
+		System.out.println(aldo.toString());
+		Student aldo2 = new Student("Aldo", "Baglio");
+		System.out.println(aldo2.toString());
+		Student aldo3 = aldo;
+		System.out.println(aldo3); // Anche se non specifico toString viene chiamato da Java
 
-		Student giovanni = new Student("Giovanni", "Storti");
-		giovanni.sayHello();
-
-		Student giacomo = aldo;
-		giacomo.surname = "Poretti";
-		giacomo.sayHello();
-
-		aldo.sayHello();
-
-		Math.sqrt(4);
-		System.out.println(Math.PI);
-
-		Student.staticMethod();
-		System.out.println(aldo.school);
 	}
 }
--- src/u4d2/Student.java
+++ src/u4d2/Student.java
@@ -4,10 +4,10 @@ public class Student {
 	// Lista attributi di istanza
 	String name;
 	String surname;
-	int age;
+	int id;
 
 	// Lista attributi statici (attributi di classe)
-	static String school = "Epicode";
+	static final String school = "Epicode";
 
 	// Lista costruttori
 	public Student(String name, String surname) {
@@ -31,5 +31,26 @@ public class Student {
 
 	static void staticMethod() {
 		System.out.println("Ciao sono un metodo statico!");
+		// System.out.println(this.name); NON posso usare this all'interno dei metodi
+		// statici
+		System.out.println(Student.school);
 	}
+
+	@Override // Annotazione per specificare che sto sovrascrivendo il comportamento di
+				// default ereditato da Object
+	public boolean equals(Object obj) { // Tutte le classi ereditano da Object una serie di metodi, tra i quali c'è
+										// equals
+		Student s = (Student) obj;
+		if (this.name == s.name && this.surname == s.surname) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", surname=" + surname + "]";
+	}
+
 }
