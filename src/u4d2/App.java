a15d0057f7b9177dfd1eb9c8ca27fd2a108bec8b
Riccardo Gulin <riccardo.gulin@gmail.com>
2023-06-27 12:59:53 +0200

    Adds Students array

--- src/u4d2/App.java
+++ src/u4d2/App.java
@@ -1,5 +1,7 @@
 package u4d2;
 
+import java.util.Arrays;
+
 public class App {
 	public static void main(String[] args) {
 //		Student aldo = new Student("Aldo", "Baglio");
@@ -55,12 +57,27 @@ public class App {
 //		}
 
 		// -------------------- TO STRING ---------------------
+//		Student aldo = new Student("Aldo", "Baglio");
+//		System.out.println(aldo.toString());
+//		Student aldo2 = new Student("Aldo", "Baglio");
+//		System.out.println(aldo2.toString());
+//		Student aldo3 = aldo;
+//		System.out.println(aldo3); // Anche se non specifico toString viene chiamato da Java
+
+		// ---------------------- ARRAY ------------------------
+
 		Student aldo = new Student("Aldo", "Baglio");
-		System.out.println(aldo.toString());
-		Student aldo2 = new Student("Aldo", "Baglio");
-		System.out.println(aldo2.toString());
-		Student aldo3 = aldo;
-		System.out.println(aldo3); // Anche se non specifico toString viene chiamato da Java
+		Student giovanni = new Student("Giovanni", "Storti");
+		Student giacomo = new Student("Giacomo", "Poretti");
+
+		Student[] students = { aldo, giovanni, giacomo };
+
+		for (int i = 0; i < students.length; i++) {
+			System.out.println(students[i]);
+		}
+
+		System.out.println(Arrays.toString(students));
 
 	}
+
 }
--- src/u4d2/Student.java
+++ src/u4d2/Student.java
@@ -53,4 +53,5 @@ public class Student {
 		return "Student [name=" + name + ", surname=" + surname + "]";
 	}
 
+
 }
